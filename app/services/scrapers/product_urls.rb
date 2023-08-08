3bb2b434b0a412e627e1e88f980823d3b93429e9
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-08-08 12:10:03 +0300

    Fixed product urls and removed fake urls

--- app/services/scrapers/product_urls.rb
+++ app/services/scrapers/product_urls.rb
@@ -1,6 +1,7 @@
 module Scrapers
   class ProductUrls
-    # URL_REGEXP = %r{\A(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
+    URL_REGEXP = %r{\A(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
+    URL_PREFIX = "https://sa3dawy.com/".freeze
 
     def initialize(source_page_id)
       @source_page = SourcePage.find source_page_id
@@ -18,7 +19,9 @@ module Scrapers
     def fetch_urls
       urls = @document.search @source_config.products_url_selector
       urls = urls.map(&:values).flatten.uniq
-      urls.map { |url| "https://sa3dawy.com/" + url }
+      urls = urls.map { |url| URL_PREFIX + url }
+      urls = urls.reject! { |url| url.include?('btn _btnread_14luk_103 btn') }
+      urls.select { |url| url.match? URL_REGEXP }
     end
 
     def fetch_document
