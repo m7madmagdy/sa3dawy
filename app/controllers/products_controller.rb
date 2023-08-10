61ada5b0997dad6de53d4fc69d9f04dd8a641c0d
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-08-10 14:24:23 +0300

    Cached products using redis

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,30 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit update destroy ]
+  CACHE_KEY = 'products#index'
+  before_action :set_product, only: %i[show edit update destroy]
+
+  before_action :check_cached, only: :index
+  after_action :cache_response, only: :index
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    if @cache_response
+      render html: @cached_response.html_safe
+    else
+      @products = Product.all.order('RANDOM()')
+    end
   end
 
   # GET /products/1 or /products/1.json
-  def show
-  end
+  def show; end
 
   # GET /products/1/edit
-  def edit
-  end
+  def edit; end
 
   # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
+        format.html { redirect_to product_url(@product), notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -32,19 +38,28 @@ class ProductsController < ApplicationController
     @product.destroy
 
     respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :price, :url, :image_url, :description, :source_page_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(:name, :price, :url, :image_url, :description, :source_page_id)
+  end
+
+  def check_cached
+    @cached_response = REDIS_CLIENT.get(CACHE_KEY)
+  end
+
+  def cache_response
+    REDIS_CLIENT.setex(CACHE_KEY, 1.minutes, response.body)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Sa3dawy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.web_console.whitelisted_ips = ['172.18.0.1']
 
     # Configuration for the application, engines, and railties goes here.
     #
